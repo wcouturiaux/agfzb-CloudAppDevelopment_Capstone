0d953ca34a1f17adfc9e610fa93e95b8cf8a96d2
WillC <wlccodes@gmail.com>
2022-01-02 05:27:13 +0000

    Add Make and Model models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.timezone import now
-
+from django.core.validators import MinValueValidator
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
@@ -9,7 +10,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=35)
+    description = models.CharField(null=False, max_length=200)
 
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,7 +25,22 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-
+class CarModel(models.Model):
+    class CarType(models.TextChoices):
+        COUPE = 'CPE', _('Coupe')
+        PICKUP = 'PKU', _('Pickup')
+        SEDAN = 'SDN', _('Sedan')
+        SUV = 'SUV'
+        WAGON = 'WGN', _('wagon')
+    
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=60)
+    dealer_id = models.IntegerField(null=False)
+    car_type = models.CharField(null=False, choices=CarType.choices, max_length=25)
+    year = models.PositiveSmallIntegerField(null=False, validators=[MinValueValidator(1900)])
+    
+    def __str__(self):
+        return (str(self.year) + " " + self.name + " " + self.car_type)
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
