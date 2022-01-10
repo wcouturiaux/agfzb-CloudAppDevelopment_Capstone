9de7d1ba7a53af64d81571f5cc394d53799f52e2
WillC <wlccodes@gmail.com>
2022-01-10 05:49:57 +0000

    Add post review api and add review view

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -29,7 +29,9 @@ def get_request(url, **kwargs):
 
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
-
+def post_request(url, json_payload, **kwargs):
+    response = requests.post(url, params=kwargs, json=json_payload)
+    return response
 
 # Create a get_dealers_from_cf method to get dealers from a cloud function
 # def get_dealers_from_cf(url, **kwargs):
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details,
         name='dealer_details'),
     # path for add a review view
+    path(route='dealer/<int:dealer_id>/add_review', view=views.add_review,
+        name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
